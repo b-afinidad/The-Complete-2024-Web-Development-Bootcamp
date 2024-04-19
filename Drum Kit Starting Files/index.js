f2b8b88405bbbc9760a5da2b28146322001ac0db
Bryan Afinidad <b.bongato.afinidad@gmail.com>
2024-04-19 12:52:13 -0700

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,15 +3,9 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 
 for (var i = 0; i<numberOfDrumButtons; i++){
-    
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    
         var buttonInnerHTML = this.innerHTML;
-
         makeSound(buttonInnerHTML);
-
-        
-    
 });
 
 }
